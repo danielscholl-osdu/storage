7305d35a5896ddeb2b223d5c73dd6b480c3d56e6
Dmitriy Novikov <7nolikov@gmail.com>
2022-01-31 16:15:13 +0400

    Updated datastore exception handling

--- provider/storage-gcp/src/main/java/org/opengroup/osdu/storage/provider/gcp/service/BatchServiceGcpImpl.java
+++ provider/storage-gcp/src/main/java/org/opengroup/osdu/storage/provider/gcp/service/BatchServiceGcpImpl.java
@@ -12,77 +12,37 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
 package org.opengroup.osdu.storage.provider.gcp.service;
-
-import com.google.cloud.datastore.DatastoreException;
-import org.opengroup.osdu.storage.logging.StorageAuditLogger;
-import org.opengroup.osdu.core.common.model.storage.DatastoreQueryResult;
-import org.opengroup.osdu.storage.provider.interfaces.IQueryRepository;
-import org.apache.http.HttpStatus;
-import org.opengroup.osdu.storage.service.BatchServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.opengroup.osdu.core.common.model.http.AppException;
 
 import static java.util.Collections.singletonList;
 
+import lombok.RequiredArgsConstructor;
+import org.opengroup.osdu.core.common.model.storage.DatastoreQueryResult;
+import org.opengroup.osdu.storage.logging.StorageAuditLogger;
+import org.opengroup.osdu.storage.provider.interfaces.IQueryRepository;
+import org.opengroup.osdu.storage.service.BatchServiceImpl;
+import org.springframework.stereotype.Service;
+
 @Service
+@RequiredArgsConstructor
 public class BatchServiceGcpImpl extends BatchServiceImpl {
 
-    @Autowired
-    private StorageAuditLogger auditLogger;
+  private final IQueryRepository queryRepository;
+  private final StorageAuditLogger auditLogger;
 
-    @Autowired
-    private IQueryRepository queryRepository;
+  @Override
+  public DatastoreQueryResult getAllKinds(String cursor, Integer limit) {
+    DatastoreQueryResult result = this.queryRepository.getAllKinds(limit, cursor);
+    this.auditLogger.readAllKindsSuccess(result.getResults());
+    return result;
+  }
 
-    @Override
-    public DatastoreQueryResult getAllKinds(String cursor, Integer limit) {
-        try {
-
-            DatastoreQueryResult result = this.queryRepository.getAllKinds(limit, cursor);
-            this.auditLogger.readAllKindsSuccess(result.getResults());
-            return result;
-        } catch (DatastoreException e) {
-            // Is invalid cursor (code = 3) exception?
-            if (e.getCode() == 3) {
-                throw this.getInvalidCursorException();
-            } else {
-                throw this.getInternalErrorException();
-            }
-        } catch (IllegalStateException e) {
-            throw this.getInvalidCursorException();
-        }
+  @Override
+  public DatastoreQueryResult getAllRecords(String cursor, String kind, Integer limit) {
+    DatastoreQueryResult result = this.queryRepository.getAllRecordIdsFromKind(kind, limit, cursor);
+    if (!result.getResults().isEmpty()) {
+      this.auditLogger.readAllRecordsOfGivenKindSuccess(singletonList(kind));
     }
-
-    @Override
-    public DatastoreQueryResult getAllRecords(String cursor, String kind, Integer limit) {
-        try {
-            DatastoreQueryResult result = this.queryRepository.getAllRecordIdsFromKind(kind, limit,
-                    cursor);
-            if (!result.getResults().isEmpty()) {
-                this.auditLogger.readAllRecordsOfGivenKindSuccess(singletonList(kind));
-            }
-            return result;
-        } catch (DatastoreException e) {
-            // TODO: Replace with OSM mapper exception
-            // Is invalid cursor (code = 3) exception?
-            if (e.getCode() == 3) {
-                throw this.getInvalidCursorException();
-            } else {
-                throw this.getInternalErrorException();
-            }
-        } catch (IllegalStateException e) {
-            throw this.getInvalidCursorException();
-        }
-    }
-    private AppException getInvalidCursorException() {
-        return new AppException(HttpStatus.SC_BAD_REQUEST, "Cursor invalid",
-                "The requested cursor does not exist or is invalid");
-    }
-
-    private AppException getInternalErrorException() {
-        return new AppException(HttpStatus.SC_INTERNAL_SERVER_ERROR, "Internal error", "An internal error occurred");
-    }
+    return result;
+  }
 }
-
