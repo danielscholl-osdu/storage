58543333d5774bc23c111c073d8fd510abddc895
Shiv Singh <sishiv@microsoft.com>
2022-03-28 17:26:12 +0000

    Updated storage service to use RedisAzureCache

--- provider/storage-azure/src/main/java/org/opengroup/osdu/storage/provider/azure/cache/SchemaRedisCache.java
+++ provider/storage-azure/src/main/java/org/opengroup/osdu/storage/provider/azure/cache/SchemaRedisCache.java
@@ -14,7 +14,8 @@
 
 package org.opengroup.osdu.storage.provider.azure.cache;
 
-import org.opengroup.osdu.core.common.cache.RedisCache;
+import org.opengroup.osdu.azure.di.RedisAzureConfiguration;
+import org.opengroup.osdu.azure.cache.RedisAzureCache;
 import org.opengroup.osdu.core.common.model.storage.Schema;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -24,15 +25,14 @@ import javax.inject.Named;
 
 @Component
 @ConditionalOnProperty(value = "runtime.env.local", havingValue = "false", matchIfMissing = true)
-public class SchemaRedisCache extends RedisCache<String, Schema> {
+public class SchemaRedisCache extends RedisAzureCache<String, Schema> {
 
     public SchemaRedisCache(
-            final @Named("REDIS_HOST") String host,
             final @Named("REDIS_PORT") int port,
-            final @Named("REDIS_PASSWORD") String password,
             final @Named("SCHEMA_REDIS_TTL") int timeout,
+            final @Named("REDIS_EXPIRATION") int expiration,
             @Value("${redis.database}") final int database)
     {
-        super(host, port, password, timeout, database, String.class, Schema.class);
+        super(String.class, Schema.class, new RedisAzureConfiguration(database, expiration, port, timeout));
     }
 }
