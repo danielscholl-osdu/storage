c6cffda9cc3697a16df6a4ad4ef5906c639d4014
Mina Otgonbold <AOtgonbold@slb.com>
2022-11-15 12:43:43 -0600

    refactor collaboration filter to its own class

---
+++ storage-core/src/test/java/org/opengroup/osdu/storage/util/CollaborationFilterTest.java
@@ -0,0 +1,61 @@
+package org.opengroup.osdu.storage.util;
+
+import org.apache.http.HttpStatus;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.opengroup.osdu.storage.response.ErrorResponse;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import static org.mockito.MockitoAnnotations.initMocks;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CollaborationFilterTest {
+    private static final String X_COLLABORATION_HEADER_NAME = "x-collaboration";
+    private static final String COLLABORATION_DIRECTIVES = "id=8e1c4e74-3b9b-4b17-a0d5-67766558ec65,application=Unit test";
+
+    @Mock
+    private HttpServletRequest httpServletRequest;
+
+    @Mock
+    private HttpServletResponse httpServletResponse;
+
+    @Mock
+    private PrintWriter writer;
+
+    @Mock
+    private FilterChain filterChain;
+
+    @InjectMocks
+    private CollaborationFilter collaborationFilter;
+
+    @Before
+    public void setup() {
+        initMocks(this);
+
+        org.springframework.test.util.ReflectionTestUtils.setField(collaborationFilter, "isCollaborationEnabled", false);
+    }
+    
+    @Test
+    public void shouldThrowException_ifCollaborationHeaderProvided_whenCollaborationFeatureFlagDisabled() throws IOException, ServletException {
+        Mockito.when(httpServletRequest.getHeader(X_COLLABORATION_HEADER_NAME)).thenReturn(COLLABORATION_DIRECTIVES);
+        Mockito.when(httpServletResponse.getWriter()).thenReturn(writer);
+
+        collaborationFilter.doFilter(httpServletRequest, httpServletResponse, filterChain);
+
+        Mockito.verify(httpServletResponse).setContentType("application/json");
+        Mockito.verify(httpServletResponse).setStatus(HttpStatus.SC_LOCKED);
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.SC_LOCKED, "Locked","Feature is not enabled on this environment");
+        Mockito.verify(writer).write(errorResponse.toString());
+    }
+}
--- storage-core/src/test/java/org/opengroup/osdu/storage/util/StorageFilterTest.java
+++ storage-core/src/test/java/org/opengroup/osdu/storage/util/StorageFilterTest.java
@@ -1,7 +1,5 @@
 package org.opengroup.osdu.storage.util;
 
-import org.apache.http.HttpStatus;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -9,57 +7,32 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.opengroup.osdu.core.common.model.http.DpsHeaders;
-import org.opengroup.osdu.core.common.model.tenant.TenantInfo;
-import org.opengroup.osdu.storage.response.ErrorResponse;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.Collections;
-import java.util.Optional;
-
-import static org.mockito.Matchers.eq;
-import static org.mockito.Mockito.when;
-import static org.mockito.MockitoAnnotations.initMocks;
 
 @RunWith(MockitoJUnitRunner.class)
 public class StorageFilterTest {
-    private static final String X_COLLABORATION_HEADER_NAME = "x-collaboration";
-    private static final String COLLABORATION_DIRECTIVES = "id=8e1c4e74-3b9b-4b17-a0d5-67766558ec65,application=Unit test";
 
-    @Mock
-    private HttpServletRequest httpServletRequest;
-
-    @Mock
-    private HttpServletResponse httpServletResponse;
-
-    @Mock
-    private PrintWriter writer;
-
-    @Mock
-    private FilterChain filterChain;
-    
     @Mock
     private DpsHeaders dpsHeaders;
 
     @InjectMocks
     private StorageFilter storageFilter;
 
-    @Before
-    public void setup() {
-        initMocks(this);
-        
-        Mockito.when(httpServletRequest.getMethod()).thenReturn("POST");
-        Mockito.when(dpsHeaders.getCorrelationId()).thenReturn("correlation-id-value");
-        org.springframework.test.util.ReflectionTestUtils.setField(storageFilter, "ACCESS_CONTROL_ALLOW_ORIGIN_DOMAINS", "custom-domain");
-        org.springframework.test.util.ReflectionTestUtils.setField(storageFilter, "isCollaborationEnabled", false);
-    }
-
     @Test
     public void shouldSetCorrectResponseHeaders() throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = Mockito.mock(HttpServletRequest.class);
+        HttpServletResponse httpServletResponse = Mockito.mock(HttpServletResponse.class);
+        FilterChain filterChain = Mockito.mock(FilterChain.class);
+        Mockito.when(dpsHeaders.getCorrelationId()).thenReturn("correlation-id-value");
+        Mockito.when(httpServletRequest.getMethod()).thenReturn("POST");
+        org.springframework.test.util.ReflectionTestUtils.setField(storageFilter, "ACCESS_CONTROL_ALLOW_ORIGIN_DOMAINS", "custom-domain");
+
         storageFilter.doFilter(httpServletRequest, httpServletResponse, filterChain);
 
         Mockito.verify(httpServletResponse).setHeader("Access-Control-Allow-Origin", "custom-domain");
@@ -75,19 +48,5 @@ public class StorageFilterTest {
         Mockito.verify(httpServletResponse).setHeader("Expires", "0");
         Mockito.verify(httpServletResponse).setHeader("correlation-id", "correlation-id-value");
         Mockito.verify(filterChain).doFilter(httpServletRequest, httpServletResponse);
-    }
-
-    @Test
-    public void shouldThrowException_ifCollaborationHeaderProvided_whenCollaborationFeatureFlagDisabled() throws IOException, ServletException {
-        Mockito.when(httpServletRequest.getHeader(X_COLLABORATION_HEADER_NAME)).thenReturn(COLLABORATION_DIRECTIVES);
-        Mockito.when(httpServletResponse.getWriter()).thenReturn(writer);
-
-        storageFilter.doFilter(httpServletRequest, httpServletResponse, filterChain);
-
-        Mockito.verify(httpServletResponse).setHeader("Access-Control-Allow-Origin", "custom-domain");
-        Mockito.verify(httpServletResponse).setContentType("application/json");
-        Mockito.verify(httpServletResponse).setStatus(HttpStatus.SC_LOCKED);
-        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.SC_LOCKED, "Locked","Feature is not enabled on this environment");
-        Mockito.verify(writer).write(errorResponse.toString());
     }
 }
