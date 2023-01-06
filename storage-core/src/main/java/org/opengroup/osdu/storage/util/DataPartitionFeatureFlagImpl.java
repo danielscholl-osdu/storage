1ed4bef520097e1f7750603c45922f1ecee5c3fa
Larissa Pereira <LPereira14@slb.com>
2023-01-06 10:45:08 -0600

    refactor to use app properties and partition based feature flags

--- storage-core/src/main/java/org/opengroup/osdu/storage/util/DataPartitionFeatureFlagImpl.java
+++ storage-core/src/main/java/org/opengroup/osdu/storage/util/DataPartitionFeatureFlagImpl.java
@@ -1,25 +1,37 @@
 package org.opengroup.osdu.storage.util;
+
 import org.opengroup.osdu.core.common.feature.FeatureFlag;
 import org.opengroup.osdu.core.common.feature.FeatureFlagFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+
 import javax.annotation.PostConstruct;
+
 @Component
-public class FeatureFlagUtil {
+public class DataPartitionFeatureFlagImpl implements ICollaborationFeatureFlag {
     @Value("${PARTITION_API}")
     private String partitionAPIEndpoint;
     @Autowired
     private FeatureFlagFactory featureFlagFactory;
+
     private FeatureFlag featureFlag;
+
     @PostConstruct
     public void init() {
         featureFlag = featureFlagFactory.create(partitionAPIEndpoint);
     }
-    public boolean isFeatureEnabled(String featureName, String dataPartitionId) {
-        return featureFlag.isEnabled(featureName, dataPartitionId);
+
+    @Override
+    public boolean isFeatureEnabled(String... featureParameters) {
+        return featureFlag.isEnabled(featureParameters[0], featureParameters[1]);
     }
-    public boolean isFeatureDisabled(String featureName, String dataPartitionId) {
-        return featureFlag.isDisabled(featureName, dataPartitionId);
+
+    @Bean(name = "dataPartitionFeatureFlag")
+    @ConditionalOnProperty(prefix = "collaboration", name = "featureFlag", havingValue = "dataPartition")
+    public ICollaborationFeatureFlag dataPartitionFeatureFlag() {
+        return new DataPartitionFeatureFlagImpl();
     }
 }
