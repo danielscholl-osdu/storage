d07040df5c80a6dc5f3783f3c3cfae4efe9bb337
Alok Joshi <AJoshi19@slb.com>
2023-02-26 22:58:29 -0600

    refactor

--- storage-core/src/main/java/org/opengroup/osdu/storage/service/PatchRecordsServiceImpl.java
+++ storage-core/src/main/java/org/opengroup/osdu/storage/service/PatchRecordsServiceImpl.java
@@ -15,6 +15,7 @@ import org.opengroup.osdu.core.common.model.storage.MultiRecordIds;
 import org.opengroup.osdu.core.common.model.storage.MultiRecordInfo;
 import org.opengroup.osdu.core.common.model.storage.Record;
 import org.opengroup.osdu.core.common.model.storage.RecordMetadata;
+import org.opengroup.osdu.core.common.model.storage.TransferInfo;
 import org.opengroup.osdu.storage.logging.StorageAuditLogger;
 import org.opengroup.osdu.storage.opa.model.ValidationOutputRecord;
 import org.opengroup.osdu.storage.opa.service.IOPAService;
@@ -29,6 +30,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.Clock;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -83,12 +85,16 @@ public class PatchRecordsServiceImpl implements PatchRecordsService {
 
     private ObjectMapper objectMapper = new ObjectMapper();
 
+    private static final String[] attributes = new String[0];
+
     @Override
     public PatchRecordsResponse patchRecords(List<String> recordIds, JsonPatch jsonPatch, String user, Optional<CollaborationContext> collaborationContext) {
-        List<String> lockedRecordsId = new ArrayList<>();
+        if(recordIds.isEmpty())
+            return PatchRecordsResponse.builder().recordCount(0).build();
+
         List<String> failedRecordIds = new ArrayList<>();
-        List<String> notFoundRecordIds;
         List<String> unauthorizedRecordIds = new ArrayList<>();
+        List<String> lockedRecordsId = new ArrayList<>();
 
         // validate record ids and metadata properties if they are being patched (acl, legalTags, kind, ancestry, etc)
         recordUtil.validateRecordIds(recordIds);
@@ -97,94 +103,46 @@ public class PatchRecordsServiceImpl implements PatchRecordsService {
         patchInputValidator.validateKind(jsonPatch);
         patchInputValidator.validateAncestry(jsonPatch);
 
-        boolean dataUpdate = isDataOrMetaBeingUpdated(jsonPatch);
-
         Map<String, String> idMap = recordIds.stream().collect(Collectors.toMap(identity(), identity()));
         List<String> idsWithoutVersion = new ArrayList<>(idMap.keySet());
 
-        if(dataUpdate) {
-            String[] attributes = {};
-            MultiRecordIds multiRecordIds = new MultiRecordIds(idsWithoutVersion, attributes);
-            //TODO: should we set a max limit of 100 records on the input list, since query records API, which also uses
-            //getMultipleRecords from batchService, has a limit of 100 records at a time
-            MultiRecordInfo multiRecordInfo = batchService.getMultipleRecords(multiRecordIds, collaborationContext);
-            notFoundRecordIds = multiRecordInfo.getInvalidRecords();
+        MultiRecordInfo multiRecordInfo = batchService.getMultipleRecords(new MultiRecordIds(idsWithoutVersion, attributes), collaborationContext);
 
-            //validate owner access before patch
-            Map<String, RecordMetadata> existingRecords = recordRepository.get(idsWithoutVersion, collaborationContext);
-            unauthorizedRecordIds = isOpaEnabled
-                    ? this.validateUserAccessAndCompliancePolicyConstraints(jsonPatch, idMap, existingRecords, user)
-                    : this.validateUserAccessAndComplianceConstraints(jsonPatch, idMap, existingRecords);
-
-            List<Record> recordsToPersist = new ArrayList<>();
-            for(Record validRecord : multiRecordInfo.getRecords()) {
-                try {
-                    JsonNode patched = jsonPatch.apply(objectMapper.convertValue(validRecord, JsonNode.class));
-                    Record patchedRecord = objectMapper.treeToValue(patched, Record.class);
-                    recordsToPersist.add(patchedRecord);
-                } catch (JsonPatchException e) {
-                    failedRecordIds.add(validRecord.getId());
-                    logger.error("Json patch exception when updating record: "+validRecord.getId(), e);
-                } catch (JsonProcessingException e) {
-                    failedRecordIds.add(validRecord.getId());
-                    logger.error("Json processing exception when updating record: "+validRecord.getId(), e);
-                }
-            }
-            //TODO: how to set lockedRecordsId? => set to empty by default
-
-            ingestionService.createUpdateRecords(false, recordsToPersist, user, collaborationContext);
-        } else {
-            List<RecordMetadata> validRecordsMetadata = new ArrayList<>();
-            List<String> validRecordsId = new ArrayList<>();
-            notFoundRecordIds = new ArrayList<>();
-            //validate owner access before patch
-            Map<String, RecordMetadata> existingRecords = recordRepository.get(idsWithoutVersion, collaborationContext);
-            unauthorizedRecordIds = isOpaEnabled
-                    ? this.validateUserAccessAndCompliancePolicyConstraints(jsonPatch, idMap, existingRecords, user)
-                    : this.validateUserAccessAndComplianceConstraints(jsonPatch, idMap, existingRecords);
-
-            final long currentTimestamp = clock.millis();
-            for (String id : idsWithoutVersion) {
-                String idWithVersion = idMap.get(id);
-                RecordMetadata metadata = existingRecords.get(CollaborationUtil.getIdWithNamespace(id, collaborationContext));
-
-                if (metadata == null) {
-                    notFoundRecordIds.add(idWithVersion);
-                    recordIds.remove(idWithVersion);
-                } else {
-                    if (unauthorizedRecordIds.contains(idWithVersion)) {
-                        recordIds.remove(idWithVersion);
-                    } else {
-                        try {
-                            objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-                            JsonNode patched = jsonPatch.apply(objectMapper.convertValue(metadata, JsonNode.class));
-                            RecordMetadata patchedRecord = objectMapper.treeToValue(patched, RecordMetadata.class);
-                            patchedRecord.setModifyUser(user);
-                            patchedRecord.setModifyTime(currentTimestamp);
-                            validRecordsMetadata.add(patchedRecord);
-                            validRecordsId.add(id);
-                        } catch (JsonPatchException e) {
-                            failedRecordIds.add(id);
-                            logger.error("Json patch exception when updating record: "+id, e);
-                        } catch (JsonProcessingException e) {
-                            failedRecordIds.add(id);
-                            logger.error("Json processing exception when updating record: "+id, e);
-                        }
-                    }
-                }
-            }
-
-            if (!validRecordsId.isEmpty()) {
-                lockedRecordsId = persistenceService.updateMetadata(validRecordsMetadata, validRecordsId, idMap, collaborationContext);
-            }
-            for (String lockedId : lockedRecordsId) {
-                recordIds.remove(lockedId);
+        List<Record> recordsToPersist = new ArrayList<>();
+        for(Record validRecord : multiRecordInfo.getRecords()) {
+            try {
+                JsonNode patched = jsonPatch.apply(objectMapper.convertValue(validRecord, JsonNode.class));
+                Record patchedRecord = objectMapper.treeToValue(patched, Record.class);
+                recordsToPersist.add(patchedRecord);
+            } catch (JsonPatchException e) {
+                recordIds.remove(validRecord.getId());
+                failedRecordIds.add(validRecord.getId());
+                logger.error("Json patch exception when updating record: "+validRecord.getId(), e);
+            } catch (JsonProcessingException e) {
+                recordIds.remove(validRecord.getId());
+                failedRecordIds.add(validRecord.getId());
+                logger.error("Json processing exception when updating record: "+validRecord.getId(), e);
             }
         }
 
+        boolean dataUpdate = isDataOrMetaBeingUpdated(jsonPatch);
+        if(dataUpdate) {
+            TransferInfo recordsUpdateResponse = ingestionService.createUpdateRecords(false, recordsToPersist, user, collaborationContext);
+            // what category is recordsUpdateResponse.getSkippedRecords()?? (skipped/failed records)
+        } else {
+            Map<String, RecordMetadata> existingRecordsMetadata = getRecordsMetadataFromRecords(multiRecordInfo.getRecords(), false, user);
+            Map<String, RecordMetadata> updatedRecordsMetadata = getRecordsMetadataFromRecords(recordsToPersist, true, user);
+            unauthorizedRecordIds = isOpaEnabled
+                    ? this.validateUserAccessAndCompliancePolicyConstraints(idMap, updatedRecordsMetadata)
+                    : this.validateUserAccessAndComplianceConstraints(jsonPatch, idMap, existingRecordsMetadata);
+            if (!recordIds.isEmpty()) {
+                lockedRecordsId = persistenceService.updateMetadata(new ArrayList<>(updatedRecordsMetadata.values()), recordIds, idMap, collaborationContext);
+                recordIds.removeAll(lockedRecordsId);
+            }
+        }
 
         PatchRecordsResponse recordsResponse = PatchRecordsResponse.builder()
-                .notFoundRecordIds(notFoundRecordIds)
+                .notFoundRecordIds(multiRecordInfo.getInvalidRecords())
                 .unAuthorizedRecordIds(unauthorizedRecordIds)
                 .recordIds(recordIds)
                 .lockedRecordIds(lockedRecordsId)
@@ -194,6 +152,26 @@ public class PatchRecordsServiceImpl implements PatchRecordsService {
         auditCreateOrUpdateRecords(recordsResponse);
 
         return recordsResponse;
+    }
+
+    private Map<String, RecordMetadata> getRecordsMetadataFromRecords(List<Record> records, boolean isUpdate, String user) {
+        Map<String, RecordMetadata> recordsMetadata = new HashMap<>();
+        for(Record record : records) {
+            RecordMetadata recordMetadata = new RecordMetadata();
+            recordMetadata.setId(record.getId());
+            recordMetadata.setAcl(record.getAcl());
+            recordMetadata.setTags(record.getTags());
+            recordMetadata.setLegal(record.getLegal());
+            recordMetadata.setAncestry(record.getAncestry());
+            recordMetadata.setKind(record.getKind());
+            if(isUpdate) {
+                long currentTimestamp = clock.millis();
+                recordMetadata.setModifyUser(user);
+                recordMetadata.setModifyTime(currentTimestamp);
+            }
+            recordsMetadata.put(record.getId(), recordMetadata);
+        }
+        return recordsMetadata;
     }
 
     private void auditCreateOrUpdateRecords(PatchRecordsResponse recordsResponse) {
@@ -216,20 +194,16 @@ public class PatchRecordsServiceImpl implements PatchRecordsService {
     }
 
     private List<String> validateUserAccessAndCompliancePolicyConstraints(
-            JsonPatch jsonPatch, Map<String, String> idMap, Map<String, RecordMetadata> existingRecords, String user) {
+            Map<String, String> idMap, Map<String, RecordMetadata> recordsMetadata) {
         List<String> unauthorizedRecordIds = new ArrayList<>();
-        List<RecordMetadata> updatedRecordsMetadata = new ArrayList<>();
-        for (String id : idMap.keySet()) {
-            RecordMetadata metadata = existingRecords.get(id);
-            if (metadata == null) continue;
 
-            //TODO: Why do we update metadata here in the original patch impl? Find out if we really need this
-            //metadata = this.recordUtil.updateRecordMetaDataForPatchOperations(metadata, jsonPatch, user, currentTimestamp);
-            updatedRecordsMetadata.add(metadata);
+        List<RecordMetadata> recordMetadataList = new ArrayList<>();
+        for (String id : idMap.keySet()) {
+            recordMetadataList.add(recordsMetadata.get(id));
         }
 
-        if (!updatedRecordsMetadata.isEmpty()) {
-            List<ValidationOutputRecord> dataAuthzResult = this.opaService.validateUserAccessToRecords(updatedRecordsMetadata, OperationType.update);
+        if (!recordMetadataList.isEmpty()) {
+            List<ValidationOutputRecord> dataAuthzResult = this.opaService.validateUserAccessToRecords(recordMetadataList, OperationType.update);
             for (ValidationOutputRecord outputRecord : dataAuthzResult) {
                 if (!outputRecord.getErrors().isEmpty()) {
                     String idWithVersion = idMap.get(outputRecord.getId());
