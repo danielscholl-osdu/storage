60174857b281770d9e877671dfe910e2a0de1182
Yurii Kondakov <ykondakov@slb.com>
2023-03-06 16:20:40 +0200

    add path validation for remove operation (do not remove values altogether)

--- storage-core/src/main/java/org/opengroup/osdu/storage/validation/ValidationDoc.java
+++ storage-core/src/main/java/org/opengroup/osdu/storage/validation/ValidationDoc.java
@@ -2,9 +2,10 @@ package org.opengroup.osdu.storage.validation;
 
 public class ValidationDoc {
     public static final String PATCH_RECORD_OPERATIONS_NOT_EMPTY = "Record patch operations cannot be empty";
-    public static final String INVALID_PATCH_PATH = "Invalid Patch Path: can only be '/acl', '/legal/legaltags', '/tags', '/kind', '/ancestry/parents', '/data' or '/meta'";
+    public static final String INVALID_PATCH_PATH = "Invalid Patch Path: can only be '/acl/viewers', 'acl/owners', '/legal/legaltags', '/tags', '/kind', '/ancestry/parents', '/data' or '/meta'";
     public static final String INVALID_PATCH_OPERATION = "Invalid Patch Operation: can only be 'replace' or 'add' or 'remove'";
     public static final String INVALID_PATCH_OPERATION_SIZE = "Invalid Patch Operation: the number of operations can only be between 1 and 100";
+    public static final String INVALID_PATCH_OPERATION_PATH_FOR_REMOVE_OPERATION = "Invalid Patch Operation: the path for remove operation must contain index of the value to be deleted";
     public static final String INVALID_PATCH_OPERATION_TYPE_FOR_KIND = "Invalid Patch Operation: for patching '/kind' only 'replace' operation is allowed";
     public static final String INVALID_PATCH_VALUES_FORMAT_FOR_KIND = "Invalid Patch Operation: for patching '/kind' only one value is allowed";
     public static final String KIND_DOES_NOT_FOLLOW_THE_REQUIRED_NAMING_CONVENTION = "Invalid kind: '%s', does not follow the required naming convention";
--- storage-core/src/main/java/org/opengroup/osdu/storage/validation/impl/JsonPatchPathValidator.java
+++ storage-core/src/main/java/org/opengroup/osdu/storage/validation/impl/JsonPatchPathValidator.java
@@ -3,6 +3,7 @@ package org.opengroup.osdu.storage.validation.impl;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.fge.jsonpatch.JsonPatch;
+import org.opengroup.osdu.storage.util.api.PatchOperations;
 import org.opengroup.osdu.storage.validation.RequestValidationException;
 import org.opengroup.osdu.storage.validation.ValidationDoc;
 import org.opengroup.osdu.storage.validation.api.ValidJsonPatchPath;
@@ -16,27 +17,59 @@ import java.util.Set;
 import java.util.function.Predicate;
 import java.util.stream.StreamSupport;
 
+import static org.opengroup.osdu.storage.util.api.PatchOperations.REMOVE;
+
 public class JsonPatchPathValidator implements ConstraintValidator<ValidJsonPatchPath, JsonPatch> {
-    private static final Set<String> VALID_PATHS = new HashSet<>(Arrays.asList("/tags", "/acl", "/legal/legaltags", "/ancestry/parents", "/kind", "/data", "/meta"));
+    private static final Set<String> VALID_PATH_BEGINNINGS = new HashSet<>(Arrays.asList("/tags", "/acl/viewers", "/acl/owners", "/legal/legaltags", "/ancestry/parents", "/kind", "/data", "/meta"));
+    private static final Set<String> INVALID_PATHS_FOR_REMOVE_OPERATION = new HashSet<>(Arrays.asList("/acl/viewers", "/acl/owners", "/legal/legaltags"));
+    private static final String PATH = "path";
+    private static final String OP = "op";
     private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     public boolean isValid(JsonPatch jsonPatch, ConstraintValidatorContext context) {
-        boolean isValid;
-        isValid = StreamSupport.stream(objectMapper.convertValue(jsonPatch, JsonNode.class).spliterator(), false)
-                .map(operation -> operation.get("path").toString().replace("\"", ""))
+        boolean isPathStartValid;
+        boolean isValidPathForRemoveOperations;
+
+        isPathStartValid = StreamSupport.stream(objectMapper.convertValue(jsonPatch, JsonNode.class).spliterator(), false)
+                .map(operation -> removeExtraQuotes(operation.get(PATH)))
                 .allMatch(getAllowedPaths());
-        if (!isValid) {
+        if (!isPathStartValid) {
             throw RequestValidationException.builder()
                     .status(HttpStatus.BAD_REQUEST)
                     .message(ValidationDoc.INVALID_PATCH_PATH)
                     .build();
         }
-        return isValid;
+
+        isValidPathForRemoveOperations = StreamSupport.stream(objectMapper.convertValue(jsonPatch, JsonNode.class).spliterator(), false)
+                .filter(removeOperation())
+                .map(operation -> removeExtraQuotes(operation.get(PATH)))
+                .noneMatch(getInvalidPathsForRemoveOperation());
+
+        if (!isValidPathForRemoveOperations) {
+            throw RequestValidationException.builder()
+                    .status(HttpStatus.BAD_REQUEST)
+                    .message(ValidationDoc.INVALID_PATCH_OPERATION_PATH_FOR_REMOVE_OPERATION)
+                    .build();
+        }
+
+        return true;
     }
 
     private Predicate<String> getAllowedPaths() {
-        return path -> VALID_PATHS.stream().anyMatch(path::startsWith);
+        return path -> VALID_PATH_BEGINNINGS.stream().anyMatch(path::startsWith);
+    }
+
+    private Predicate<JsonNode> removeOperation() {
+        return operation -> REMOVE.equals(PatchOperations.forOperation(removeExtraQuotes(operation.get(OP))));
+    }
+
+    private Predicate<String> getInvalidPathsForRemoveOperation() {
+        return path -> INVALID_PATHS_FOR_REMOVE_OPERATION.stream().anyMatch(path::equals);
+    }
+
+    private String removeExtraQuotes(JsonNode jsonNode) {
+        return jsonNode.toString().replace("\"", "");
     }
 
 }
