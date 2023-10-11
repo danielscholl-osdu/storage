58d67c5f3c99d55234e3a8f9f94ecb453eee6f72
Deepa Kumari <deepa_kumari@epam.com>
2023-10-11 10:24:40 +0000

    Handling validation and error in storage filters

--- storage-core/src/test/java/org/opengroup/osdu/storage/util/StorageFilterTest.java
+++ storage-core/src/test/java/org/opengroup/osdu/storage/util/StorageFilterTest.java
@@ -1,28 +1,40 @@
 package org.opengroup.osdu.storage.util;
 
+import org.apache.http.HttpStatus;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.opengroup.osdu.core.common.exception.UnauthorizedException;
+import org.opengroup.osdu.core.common.model.http.AppError;
+import org.opengroup.osdu.core.common.model.http.AppException;
 import org.opengroup.osdu.core.common.model.http.DpsHeaders;
+import org.springframework.core.annotation.Order;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Collections;
+import java.io.PrintWriter;
+import java.util.Arrays;
+
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
-public class StorageFilterTest {
+public class StorageFilterTest extends BaseOsduFilter{
 
     @Mock
     private DpsHeaders dpsHeaders;
 
     @InjectMocks
     private StorageFilter storageFilter;
+    @Mock
+    private PrintWriter writer;
 
     @Test
     public void shouldSetCorrectResponseHeaders() throws IOException, ServletException {
@@ -31,6 +43,11 @@ public class StorageFilterTest {
         FilterChain filterChain = Mockito.mock(FilterChain.class);
         Mockito.when(dpsHeaders.getCorrelationId()).thenReturn("correlation-id-value");
         Mockito.when(httpServletRequest.getMethod()).thenReturn("POST");
+        Mockito.when(dpsHeaders.getPartitionId()).thenReturn("opendes");
+        Mockito.when(dpsHeaders.getAuthorization()).thenReturn("token ");
+        when(httpServletRequest.getRequestURI()).thenReturn("https://my-service-url/api/storage/v2/");
+        when(httpServletRequest.getContextPath()).thenReturn("/api/storage/v2/");
+        ReflectionTestUtils.setField(storageFilter, "excludedPaths", Arrays.asList("info", "swagger", "health", "api-docs"));
         org.springframework.test.util.ReflectionTestUtils.setField(storageFilter, "ACCESS_CONTROL_ALLOW_ORIGIN_DOMAINS", "custom-domain");
 
         storageFilter.doFilter(httpServletRequest, httpServletResponse, filterChain);
@@ -48,5 +65,61 @@ public class StorageFilterTest {
         Mockito.verify(httpServletResponse).setHeader("Expires", "0");
         Mockito.verify(httpServletResponse).setHeader("correlation-id", "correlation-id-value");
         Mockito.verify(filterChain).doFilter(httpServletRequest, httpServletResponse);
+    }
+    @Test
+    public void shouldReturnErrorWhenMissingDataPartitionIdHeader() throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = Mockito.mock(HttpServletRequest.class);
+        HttpServletResponse httpServletResponse = Mockito.mock(HttpServletResponse.class);
+        FilterChain filterChain = Mockito.mock(FilterChain.class);
+        Mockito.when(dpsHeaders.getCorrelationId()).thenReturn("correlation-id-value");
+        Mockito.when(httpServletRequest.getMethod()).thenReturn("POST");
+        Mockito.when(httpServletResponse.getWriter()).thenReturn(writer);
+        Mockito.when(dpsHeaders.getAuthorization()).thenReturn("token");
+        org.springframework.test.util.ReflectionTestUtils.setField(storageFilter, "ACCESS_CONTROL_ALLOW_ORIGIN_DOMAINS", "custom-domain");
+        when(httpServletRequest.getRequestURI()).thenReturn("https://my-service-url/api/storage/v2/");
+        when(httpServletRequest.getContextPath()).thenReturn("/api/storage/v2/");
+        ReflectionTestUtils.setField(storageFilter, "excludedPaths", Arrays.asList("info", "swagger", "health", "api-docs"));
+        storageFilter.doFilter(httpServletRequest, httpServletResponse, filterChain);
+        AppError error = new AppError(HttpStatus.SC_BAD_REQUEST, "Bad Request", "data-partition-id header is missing");
+        Mockito.verify(writer).write(appErrorToJson(error));
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenMissingAuthHeader() throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = Mockito.mock(HttpServletRequest.class);
+        HttpServletResponse httpServletResponse = Mockito.mock(HttpServletResponse.class);
+        FilterChain filterChain = Mockito.mock(FilterChain.class);
+        Mockito.when(dpsHeaders.getCorrelationId()).thenReturn("correlation-id-value");
+        Mockito.when(httpServletRequest.getMethod()).thenReturn("POST");
+        Mockito.when(httpServletResponse.getWriter()).thenReturn(writer);
+        org.springframework.test.util.ReflectionTestUtils.setField(storageFilter, "ACCESS_CONTROL_ALLOW_ORIGIN_DOMAINS", "custom-domain");
+        when(httpServletRequest.getRequestURI()).thenReturn("https://my-service-url/api/storage/v2/");
+        when(httpServletRequest.getContextPath()).thenReturn("/api/storage/v2/");
+        ReflectionTestUtils.setField(storageFilter, "excludedPaths", Arrays.asList("info", "swagger", "health", "api-docs"));
+        storageFilter.doFilter(httpServletRequest, httpServletResponse, filterChain);
+        AppError error = new AppError(HttpStatus.SC_UNAUTHORIZED, "Unauthorized", "Authorization header is missing");
+        Mockito.verify(writer).write(appErrorToJson(error));
+    }
+
+    @Test
+    public void ShouldCatchExceptionsThrownFromDoFilter() throws ServletException, IOException {
+        HttpServletRequest httpServletRequest = Mockito.mock(HttpServletRequest.class);
+        HttpServletResponse httpServletResponse = Mockito.mock(HttpServletResponse.class);
+        FilterChain filterChain = Mockito.mock(FilterChain.class);
+        Mockito.when(dpsHeaders.getCorrelationId()).thenReturn("correlation-id-value");
+        Mockito.when(httpServletRequest.getMethod()).thenReturn("POST");
+        Mockito.when(httpServletResponse.getWriter()).thenReturn(writer);
+        org.springframework.test.util.ReflectionTestUtils.setField(storageFilter, "ACCESS_CONTROL_ALLOW_ORIGIN_DOMAINS", "custom-domain");
+        when(httpServletRequest.getRequestURI()).thenReturn("https://my-service-url/api/storage/v2/");
+        when(httpServletRequest.getContextPath()).thenReturn("/api/storage/v2/");
+        Mockito.when(dpsHeaders.getAuthorization()).thenReturn("token");
+        Mockito.when(dpsHeaders.getPartitionId()).thenReturn("data-partition");
+        ReflectionTestUtils.setField(storageFilter, "excludedPaths", Arrays.asList("info", "swagger", "health", "api-docs"));
+
+        doThrow(new AppException(HttpStatus.SC_LOCKED, "Locked", "Feature is not enabled on this environment")).when(filterChain).doFilter(httpServletRequest, httpServletResponse);
+
+        storageFilter.doFilter(httpServletRequest, httpServletResponse, filterChain);
+        AppError error = new AppError(HttpStatus.SC_LOCKED, "Locked", "Feature is not enabled on this environment");
+        Mockito.verify(writer).write(appErrorToJson(error));
     }
 }
