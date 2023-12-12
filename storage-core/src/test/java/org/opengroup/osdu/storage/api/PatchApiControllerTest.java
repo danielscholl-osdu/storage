48e4d1d060786ed71fffc3ae5e9167776648f231
konradkrasno <konrad.krasnodebski@hitachids.com>
2023-12-12 10:25:51 +0100

    Simplify cache

--- storage-core/src/test/java/org/opengroup/osdu/storage/api/PatchApiControllerTest.java
+++ storage-core/src/test/java/org/opengroup/osdu/storage/api/PatchApiControllerTest.java
@@ -52,7 +52,7 @@ import static org.mockito.ArgumentMatchers.eq;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = PatchApi.class)
-@ComponentScan({"org.opengroup.osdu", "org.opengroup.primary"})
+@ComponentScan("org.opengroup.osdu")
 public class PatchApiControllerTest extends ApiTest<PatchRecordsRequestModel> {
 
     private final ObjectMapper mapper = new ObjectMapper();
