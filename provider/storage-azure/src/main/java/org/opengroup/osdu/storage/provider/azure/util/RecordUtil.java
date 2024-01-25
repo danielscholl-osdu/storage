5044a21d293c7ac116f836172d9225d7b716d4b8
NThakur4 <nthakur4@slb.com>
2024-01-25 14:56:09 -0600

    Revert "Merged PR 25612: Reject record ids with unsupported characters at the end"
    This reverts commit e356f2b7a68c41be0ecde5e8d92dc91d30c28e82.

--- provider/storage-azure/src/main/java/org/opengroup/osdu/storage/provider/azure/util/RecordUtil.java
+++ provider/storage-azure/src/main/java/org/opengroup/osdu/storage/provider/azure/util/RecordUtil.java
@@ -1,5 +1,7 @@
 package org.opengroup.osdu.storage.provider.azure.util;
 
+import static org.apache.commons.lang3.StringUtils.isNoneBlank;
+
 import org.apache.http.HttpStatus;
 import org.opengroup.osdu.core.common.model.http.AppException;
 import org.opengroup.osdu.core.common.model.storage.RecordMetadata;
@@ -8,50 +10,30 @@ import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Objects;
-import java.util.function.Predicate;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import static org.apache.commons.lang3.StringUtils.isNoneBlank;
 
 @Component
 public class RecordUtil {
 
     @Value("${record-id.max.length}")
     private Integer recordIdMaxLength;
-    private static final Pattern endingCharacterPattern = Pattern.compile(".*[\\.\\\\\\/]$");
-    private static final String UNSUPPORTED_CHARACTER_ERROR_MESSAGE = "RecordId values ending in dot (.), backslash (\\), or forward slash (/) not allowed";
-    private static final String MAX_LENGTH_ERROR_MESSAGE = "RecordId values which are exceeded 100 symbols temporarily not allowed";
 
     public void validateIds(List<String> inputRecords) {
         if (inputRecords.stream().filter(Objects::nonNull)
                 .anyMatch(id -> id.length() > recordIdMaxLength)) {
-
-            throw new AppException(HttpStatus.SC_BAD_REQUEST, "Invalid id", MAX_LENGTH_ERROR_MESSAGE);
-        }
-        List<String> recordIds = inputRecords.stream().filter(Objects::nonNull).toList();
-        for (String record : recordIds) {
-            Matcher recordIdMatcher = endingCharacterPattern.matcher(record);
-            boolean matchFound = recordIdMatcher.find();
-            if (matchFound) {
-                throw new AppException(HttpStatus.SC_BAD_REQUEST, "Invalid id", UNSUPPORTED_CHARACTER_ERROR_MESSAGE);
-            }
+            String msg = "RecordId values which are exceeded 100 symbols temporarily not allowed";
+            throw new AppException(HttpStatus.SC_BAD_REQUEST, "Invalid id", msg);
         }
     }
 
-    public String getKindForVersion(RecordMetadata recordMetadata, String version) {
-        String gcsVersionPath =
-                recordMetadata.getGcsVersionPaths()
+    public String getKindForVersion(RecordMetadata record, String version) {
+        String versionPath =
+                record.getGcsVersionPaths()
                         .stream()
-                        .filter(isGcsVersionPathEndsWith(version))
+                        .filter(path -> isNoneBlank(path) && path.contains(version))
                         .findFirst()
-                        .orElseThrow(() -> throwVersionNotFound(recordMetadata.getId(), version));
+                        .orElseThrow(() -> throwVersionNotFound(record.getId(), version));
 
-        return gcsVersionPath.split("/")[0];
-    }
-
-    private static Predicate<String> isGcsVersionPathEndsWith(String version) {
-        return gcsVersionPath -> isNoneBlank(gcsVersionPath) && gcsVersionPath.endsWith("/" + version);
+        return versionPath.split("/")[0];
     }
 
     private AppException throwVersionNotFound(String id, String version) {
