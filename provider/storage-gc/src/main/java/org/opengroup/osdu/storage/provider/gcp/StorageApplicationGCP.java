ffe6dfc2ff3653012ea94c135dbf45d93144bcb9
Riabokon Stanislav(EPAM)[GCP] <stanislav_riabokon@epam.com>
2025-02-21 15:29:16 +0000

    Moved GC to CImpl (GONRG-10420)

--- provider/storage-gc/src/main/java/org/opengroup/osdu/storage/provider/gcp/StorageApplicationGCP.java
+++ provider/storage-gc/src/main/java/org/opengroup/osdu/storage/provider/gcp/StorageApplicationGCP.java
@@ -1,6 +1,6 @@
 /*
- *  Copyright 2020-2022 Google LLC
- *  Copyright 2020-2022 EPAM Systems, Inc
+ *  Copyright 2020-2025 Google LLC
+ *  Copyright 2020-2025 EPAM Systems, Inc
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
