6f5296e117972824445493d9466f9cb7baf2d3e3
Rustam Lotsmanenko (EPAM) <rustam_lotsmanenko@epam.com>
2025-04-24 07:06:45 +0000

    Fix rollback to remove orphaned metadata on insert failure

--- storage-core/src/main/java/org/opengroup/osdu/storage/provider/interfaces/IRecordsMetadataRepository.java
+++ storage-core/src/main/java/org/opengroup/osdu/storage/provider/interfaces/IRecordsMetadataRepository.java
@@ -14,14 +14,12 @@
 
 package org.opengroup.osdu.storage.provider.interfaces;
 
+import com.github.fge.jsonpatch.JsonPatch;
 import java.io.Serializable;
 import java.util.AbstractMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-
-
-import com.github.fge.jsonpatch.JsonPatch;
 import org.apache.commons.lang3.NotImplementedException;
 import org.opengroup.osdu.core.common.model.http.CollaborationContext;
 import org.opengroup.osdu.core.common.model.legal.LegalCompliance;
@@ -37,6 +35,10 @@ public interface IRecordsMetadataRepository<K extends Serializable> {
     List<RecordMetadata> createOrUpdate(List<RecordMetadata> recordsMetadata, Optional<CollaborationContext> collaborationContext);
 
     void delete(String id, Optional<CollaborationContext> collaborationContext);
+
+    default void batchDelete(List<String> ids, Optional<CollaborationContext> collaborationContext){
+        ids.forEach(id -> delete(id, collaborationContext));
+    }
 
     RecordMetadata get(String id, Optional<CollaborationContext> collaborationContext);
 
